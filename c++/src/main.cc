1bf7402b555a5057520d4687145e9f90b745169f
Dominique MICHEL <dominique.michel@epita.fr>
2020-05-14 20:34:56 +0200

    [C++][ADD] added final cmake and main version

--- c++/src/main.cc
+++ c++/src/main.cc
@@ -1,33 +1,40 @@
-#include <iostream>
-#include "argparse/argparse.hh"
-#include "loader/data-loader.hh"
-#include "loader/data-bank.hh"
-#include "converter/converter.hh"
-#include "scheduler/scheduler.hh"
+#include "main.hh"
+
+namespace po = boost::program_options;
 
 int main(int argc, char** argv)
 {
-    try
-    {
-        // Handle parsing
-        argparse::Argparse args(argc, argv);
-
-        // Parse JSON file
-        loader::DataBank bank;
-        loader::DataLoader dataLoader(args.json_path_get());
-        dataLoader.load_data(bank);
-
-        // Reformat easier-manipulating data
-        converter::Converter converter(bank);
-
-        // compute output
-        scheduler::Scheduler scheduler(converter);
-        scheduler.convert_defences();
-    }
-    catch (std::runtime_error& e)
-    {
-        std::cerr << "[ERROR] " << e.what();
-        return 1;
+    try {
+        auto const& desc = define_options();
+        auto const& vm = parse_options(desc, argc, argv);
+        run(desc, vm);
+    } catch (po::error &e) {
+        std::cerr << "[ERROR] " << e.what() << std::endl;
+        return 2;
     }
     return 0;
+}
+
+po::options_description define_options()
+{
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "show usage");
+    return desc;
+}
+
+po::variables_map parse_options(const po::options_description& desc, int argc,
+        char** argv)
+{
+    po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+    return vm;
+}
+
+void run(const po::options_description& desc, const po::variables_map& vm)
+{
+    if (vm.count("help"))
+        std::cout << desc << std::endl;
 }
